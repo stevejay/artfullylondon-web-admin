a8f45d54e15da63bea4e4041ee1f06ed8be88a5b
Steve Johns <steve@stevejay.net>
2018-03-04 20:51:22 +0000

    set jest to run in band on circle ci

--- cypress/integration/example_spec.js
+++ cypress/integration/example_spec.js
@@ -1,5 +1,6 @@
-describe('My First Test', function () {
-  it('Does not do much!', function () {
-    expect(true).to.equal(false)
+describe('initial load', function () {
+  it('visits the site', function () {
+    cy.visit('/')
+    cy.contains('Log In')
   })
 })
